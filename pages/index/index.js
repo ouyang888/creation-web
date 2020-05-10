542a44a8b20d888e5ae6217e9f69523e8038652c
2271656075@qq.com <2271656075@qq.com>
2020-05-11 01:03:03 +0800

    updata-首页

--- pages/index/index.js
+++ pages/index/index.js
@@ -6,9 +6,12 @@ Page({
     tabList:['推荐套餐','窗/防盗网','电视背景','乳胶漆','天花','门锁'],
     //左侧栏-当前选中值
     leftTabActived:0,
+    servenImg:''
   },
   onLoad: function () {
-    
+    this.setData({
+      servenImg: app.uploadImg.url,
+    })
   },
   handleTab(index){
     this.setData({
